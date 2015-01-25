d5b6626622235bbdafe9d825fcfc9e9b57567c6a
Kristen <kawilliams3@gmail.com>
2015-01-24 20:49:05 -0600

    got "skateboard" working

--- js/app.js
+++ js/app.js
@@ -15,6 +15,49 @@ function app(){
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
+
+        var id = function(element){
+
+            return document.getElementById(element);
+        };
+
+        var girl = id('girl');
+        var ground = id ('ground');
+        var groundPosition = {
+            x:0,
+            y:0
+        };
+        var girlPosition = {
+        x:20 ,
+        y: ground.offsetHeight}; //so that start above ground
+
+        var plotObject = function(obj, coordinates) { //coordinates is an array
+        // changed origin point from top left to be congruent with a classical schema
+        //relocated origin point of placed objects to the bottom left
+            obj.style.left = coordinates.x + 'px';
+            obj.style.top = document.querySelector('body').offsetHeight - coordinates.y - obj.offsetHeight + 'px';
+
+        };
+
+        plotObject(ground , groundPosition);
+        plotObject(girl, girlPosition);
+
+        document.onkeydown=function(e){
+            var evtobj = window.event? event : e;
+
+            if(evtobj.keyCode==37) {  //left arrow pressed
+                girlPosition.x += -1; //change x coordinate of girl by negative one
+            }
+
+            if(evtobj.keyCode==39){  //right arrow pressed
+                girlPosition.x += 1; //change x coordinate of girl by positive one
+            }
+
+            plotObject(girl, girlPosition);
+        }
+
+
+
     })
 
 }
