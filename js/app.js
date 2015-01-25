e57089ff8743c95af397876ceb54e7d0972b509d
Kristen <kawilliams3@gmail.com>
2015-01-25 12:56:00 -0600

    uptojumping

--- js/app.js
+++ js/app.js
@@ -1,62 +1,105 @@
 window.onload = app;
 
 // runs when the DOM is loaded
-function app(){
+function app() {
     "use strict";
 
     // load some scripts (uses promises :D)
     loader.load(
         //css
-        {url: "./dist/style.css"},
+        {
+            url: "./dist/style.css"
+        },
         //js
-        {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
+        {
+            url: "./bower_components/jquery/dist/jquery.min.js"
+        }, {
+            url: "./bower_components/lodash/dist/lodash.min.js"
+        }
         // {url: "./bower_components/backbone/backbone.js"}
-    ).then(function(){
+    ).then(function() {
         document.querySelector("html").style.opacity = 1;
         // start app?
 
-        var id = function(element){
+        var id = function(element) {
 
             return document.getElementById(element);
         };
 
-        var girl = id('girl');
-        var ground = id ('ground');
+
+
+        var ground = id('ground');
+
         var groundPosition = {
-            x:0,
-            y:0
+            x: 0,
+            y: 0
         };
-        var girlPosition = {
-        x:20 ,
-        y: ground.offsetHeight}; //so that start above ground
+
+        var girl = {
+            id: id('girl'),
+            x: 20,
+            y: ground.offsetHeight,
+            velocity: 20
+        }; //so start above ground
+
 
         var plotObject = function(obj, coordinates) { //coordinates is an array
-        // changed origin point from top left to be congruent with a classical schema
-        //relocated origin point of placed objects to the bottom left
+            // changed origin point from top left to be congruent with a classical schema
+            //relocated origin point of placed objects to the bottom left
             obj.style.left = coordinates.x + 'px';
             obj.style.top = document.querySelector('body').offsetHeight - coordinates.y - obj.offsetHeight + 'px';
 
         };
 
-        plotObject(ground , groundPosition);
-        plotObject(girl, girlPosition);
+        plotObject(ground, groundPosition);
+        plotObject(girl.id, girl);
 
-        document.onkeydown=function(e){
-            var evtobj = window.event? event : e;
+        document.onkeydown = function(e) {
+            var evtobj = window.event ? event : e;
 
-            if(evtobj.keyCode==37) {  //left arrow pressed
-                girlPosition.x += -1; //change x coordinate of girl by negative one
+            if (evtobj.keyCode == 37) { //left arrow pressed
+                girl.x += -5; //change x coordinate of girl by negative one
             }
 
-            if(evtobj.keyCode==39){  //right arrow pressed
-                girlPosition.x += 1; //change x coordinate of girl by positive one
+            if (evtobj.keyCode == 39) { //right arrow pressed
+                girl.x += 5; //change x coordinate of girl by positive one
             }
 
-            plotObject(girl, girlPosition);
+            // if(evtobj.keyCode==38) {  //up arrow pressed
+            //     girlPosition.y += 5;
+            // }
+
+            // if(evtobj.keyCode==40?) {  //down arrow pressed
+            //     girlPosition.y += 5;
+            // }
+
+            plotObject(girl.id, girl);
         }
 
+        document.onkeyup = function() { //function fires one at a time instead of keydown  all the time
+            var evtobj = window.event ? event : e
 
+
+            if (evtobj.keyCode == 38 && girl.velocity === 20) { //allowed to move if v == 20
+
+
+                var gravity = -1;
+
+                var jump = setInterval(function() {
+                    girl.y += girl.velocity;
+                    plotObject(girl.id, girl);
+                    girl.velocity += gravity;
+
+                    if (girl.velocity <= -21) {
+                        girl.velocity = 20
+                        clearInterval(jump)
+                    }
+                }, 50); //up arrow pressed
+
+            }
+
+
+        }
 
     })
 
